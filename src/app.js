cc93709456a37a8792a337867182da9db700f409
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2023-07-29 19:37:36 -0300

    cambios menores

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 3000;
 const userRoutes = require('./routes/userRoutes');
 
+app.use(express.json());
+
+
 app.use('/users', userRoutes);
 
-app.use(express.json());
 
 
 
